92102fc478f673dceb4bee6e99bffc2ba232f2aa
zenandehlaula-lang <zenande.hlaula@umuzi.org>
2025-12-07 01:22:26 +0200

    Update Create_DB_Messy_sales.sql

---
+++ Assignment 2 SQL/Dirty_cafe_sales_cleaning_project/SQL files/Create_DB_Messy_sales.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS `cafe_sales`;
+CREATE DATABASE `cafe_sales`;
+USE `cafe_sales`;
+
+
+CREATE TABLE dirty_cafe_sales (
+  Transaction_ID VARCHAR(30),
+  Item VARCHAR(30),
+  Quantity INT,
+  Price_per_unit INT,
+  Total_spent INT,
+  Location VARCHAR (30),
+  Transaction_date DATE
+  );
+
+LOAD DATA LOCAL INFILE ''
+INTO TABLE dirty_cafe_sales
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`Transaction ID`, `Item`, `Quantity`, `Price Per Unit`,
+ `Total Spent`, `Payment Method`, `Location`, `Transaction Date`);
