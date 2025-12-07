f58ec09273fc701a09a73bd6a29f49a5555f6076
zenandehlaula-lang <zenande.hlaula@umuzi.org>
2025-12-07 20:52:56 +0200

    Update Staging table.sql

--- Assignment 2 SQL/Dirty_cafe_sales_cleaning_project/SQL files/Staging table.sql
+++ Assignment 2 SQL/Dirty_cafe_sales_cleaning_project/SQL files/Staging table.sql
@@ -4,6 +4,7 @@ CREATE TABLE clean_cafe_sales AS
 SELECT *
 FROM dirty_cafe_sales;
 
+SET SQL_SAFE_UPDATES = 0;
 -- Fix Item based on Price
 UPDATE clean_cafe_sales c
 JOIN ref.item_prices r
@@ -17,3 +18,5 @@ JOIN ref.item_prices r
     ON c.Item = r.item
 SET c.Price_Per_Unit = r.Price_Per_Unit
 WHERE c.Item IS NOT NULL;
+
+SET SQL_SAFE_UPDATES = 1;
