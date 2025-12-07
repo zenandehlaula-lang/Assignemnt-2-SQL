58b464c3812b545114d1e64263f63aa2a3fb7929
zenandehlaula-lang <zenande.hlaula@umuzi.org>
2025-12-07 20:33:00 +0200

    Update Create reference tables.sql

--- Assignment 2 SQL/Dirty_cafe_sales_cleaning_project/SQL files/Create reference tables.sql
+++ Assignment 2 SQL/Dirty_cafe_sales_cleaning_project/SQL files/Create reference tables.sql
@@ -9,3 +9,23 @@ WHERE `Price Per Unit` REGEXP '^[0-9]+(\\.[0-9]+)?$'
   AND Item IS NOT NULL
   AND Item NOT IN ('UNKNOWN', 'ERROR')
 GROUP BY Item;
+
+DROP TABLE IF EXISTS clean_cafe_sales;
+
+CREATE TABLE clean_cafe_sales AS
+SELECT *
+FROM dirty_cafe_sales;
+
+-- Fix Item based on Price
+UPDATE clean_cafe_sales c
+JOIN ref.item_prices r
+    ON c.Price_Per_Unit = r.Price_Per_Unit
+SET c.Item = r.Item
+WHERE c.Price_Per_Unit IS NOT NULL;
+
+-- Fix Price based on Item
+UPDATE clean_cafe_sales c
+JOIN ref.item_prices r
+    ON c.Item = r.item
+SET c.Price_Per_Unit = r.Price_Per_Unit
+WHERE c.Item IS NOT NULL;
