cd28047995d8c960cc2baa0dfb13341a1022f393
zenandehlaula-lang <zenande.hlaula@umuzi.org>
2025-12-07 10:59:15 +0200

    Update Create reference tables.sql

---
+++ Assignment 2 SQL/Dirty_cafe_sales_cleaning_project/SQL files/Create reference tables.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS clean_item_price;
+
+CREATE TABLE clean_item_price AS
+SELECT
+    Item,
+    MIN(CAST(`Price Per Unit` AS DECIMAL(10,2))) AS Price_Per_Unit
+FROM dirty_cafe_sales
+WHERE `Price Per Unit` REGEXP '^[0-9]+(\\.[0-9]+)?$'
+  AND Item IS NOT NULL
+  AND Item NOT IN ('UNKNOWN', 'ERROR')
+GROUP BY Item;
